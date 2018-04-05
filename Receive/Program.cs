10a538636235460dc2bb088ae22d77243ab79f05
iOgre <igorvadimovich@outlook.com>
2018-04-05 20:00:20 +0500

    Initial commit

---
+++ Receive/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Receive
+{
+    class Program
+    {
+        private const string routingKey = "task_queue";
+        public static void Main()
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(routingKey, false, false, false, null);
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (sender, args) =>
+                    {
+                        var message = Encoding.UTF8.GetString(args.Body);
+                        Console.WriteLine($"Received {message}");
+                    };
+                    channel.BasicConsume("hello", true, consumer);
+                    Console.WriteLine("Press ENTER to exit");
+                    Console.ReadLine();
+                }
+                
+            }
+          
+        }
+    }
+}
---
+++ Send/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Send
+{
+    class Program
+    {
+        private const string routingKey = "task_queue";
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+            int attempt = 0;
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                    do
+                    {
+                        string message = $"Hello World {attempt++}";
+                        var body = Encoding.UTF8.GetBytes(message);
+                        var properties = channel.CreateBasicProperties();
+                        properties.Persistent = true;
+                        channel.BasicPublish(exchange: "", routingKey: routingKey, basicProperties: null, body: body);
+                        Console.WriteLine($"message {message} sent");
+                    } while (Console.ReadLine() != "q");
+                }
+            }
+
+            Console.WriteLine("Press Enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
